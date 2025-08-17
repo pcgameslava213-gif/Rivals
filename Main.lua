eaab64b84c9a0c8550994f852c6c9ccfc3d2c144
pcgameslava213-gif <pcgameslava213@gmail.com>
2025-08-17 14:58:19 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,2 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/pcgameslava213-gif/Rivals/refs/heads/main/Script1.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/pcgameslava213-gif/Rivals/refs/heads/main/Rainbow.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/pcgameslava213-gif/Rivals/refs/heads/main/Script2.lua"))()
